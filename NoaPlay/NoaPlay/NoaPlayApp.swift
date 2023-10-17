db3895cfd9384b9d6675eddb63904be33135be1e
Alberto Novo <noveiras@gmail.com>
2023-10-17 16:30:54 -0400

    Create xcode project

---
+++ NoaPlay/NoaPlay/NoaPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoaPlayApp.swift
+//  NoaPlay
+//
+//  Created by Alberto Novo Garrido on 2023-10-17.
+//
+
+import SwiftUI
+
+@main
+struct NoaPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
